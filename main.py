041d5bfdf98762a20e6578e13b1084fe1da49b61
serity93 <sean.ricardo.tyler@gmail.com>
2018-05-13 01:50:09 -0700

    Add basic member commands where bot replies with member name

--- main.py
+++ main.py
@@ -21,8 +21,43 @@ async def on_ready():
     print(bot.user.id)
     print('--------')
 
-@bot.command()
-async def helloworld(ctx):
-    await ctx.send("Hello world!")
+@bot.command(pass_context=True)
+async def helloworld(context):
+    await context.send("Hello world!")
+
+@bot.command(name='solji',
+             description='Posts a random Solji pic.',
+             aliases=['soulg'],
+             pass_context=True)
+async def solji(context):
+    await context.send('Solji!')
+
+@bot.command(name='le',
+             description='Posts a random LE pic.',
+             aliases=['hyojin', 'elly'],
+             pass_context=True)
+async def le(context):
+    await context.send('LE!')
+
+@bot.command(name='hani',
+             description='Posts a random Hani pic.',
+             aliases=['heeyeon'],
+             pass_context=True)
+async def hani(context):
+    await context.send('Hani!')
+
+@bot.command(name='hyelin',
+             description='Posts a random Hyelin pic.',
+             aliases=['hyerin'],
+             pass_context=True)
+async def hyelin(context):
+    await context.send('Hyelin!')
+
+@bot.command(name='jeonghwa',
+             description='Posts a random Jeonghwa pic.',
+             aliases=['junghwa'],
+             pass_context=True)
+async def jeonghwa(context):
+    await context.send('Jeonghwa!')
 
 bot.run(constants.TOKEN)
