32903b6aae830f11c0e179b8a4cbb061908d3d9b
serity93 <sean.ricardo.tyler@gmail.com>
2018-05-13 01:41:06 -0700

    Add basic discord logging

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 import constants
 import discord
+import logging
+
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix='.', description='A bot created for the EXID Discord server.')
+bot = commands.Bot(command_prefix='.',
+                   description='A bot created for the EXID Discord server.',
+                   case_insensitive=True)
+
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
 
 @bot.event
 async def on_ready():
