85e6d45a3fce5c5af56af1acb1a851e0003c78f5
serity93 <sean.ricardo.tyler@gmail.com>
2018-07-17 23:54:21 -0700

    Implement remove message command

--- cogs/json_data.py
+++ cogs/json_data.py
@@ -34,6 +34,26 @@ class JsonData:
         
         await context.send(self.list_leave_messages())
 
+    @leave_messages.command(
+        name='remove',
+        description='Removes a leave message.',
+        pass_context=True)
+    async def remove(self, context, index:int):
+        with open(JSON_DATA_FILE, "r") as file:
+            json_data = json.load(file)
+        json_messages = json_data[LEAVE_MESSAGES]
+
+        if index < 1 or index > len(json_messages):
+            await context.send('Invalid index!')
+            return
+
+        json_messages.pop(index-1)
+        json_data[LEAVE_MESSAGES] = json_messages
+
+        with open(JSON_DATA_FILE, "w") as outfile:
+            json.dump(json_data, outfile)
+        
+        await context.send(self.list_leave_messages())
 
     def list_leave_messages(self):
         with open(JSON_DATA_FILE, "r") as file:
@@ -41,7 +61,7 @@ class JsonData:
         json_messages = json_data[LEAVE_MESSAGES]
 
         output = '```\n'
-        index = 0
+        index = 1
         for message in json_messages:
             output += f'({index}) {message}\n'
             index += 1
