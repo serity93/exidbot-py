5aa83b9d71de81d9fa001435f53d05dc9868f75c
serity93 <sean.ricardo.tyler@gmail.com>
2018-08-20 23:04:51 -0700

    Add check for Mods role to mod commands

--- cogs/mod.py
+++ cogs/mod.py
@@ -7,6 +7,7 @@ class Mod:
 
   @commands.command(name='say',
     description='Sends a message to the channel.')
+  @commands.has_role("Mods")
   async def say(self, context, channel:discord.TextChannel, *, message:str = None):
     if message is None:
       await context.send('Please enter a message for me to send!')
@@ -16,6 +17,7 @@ class Mod:
   
   @commands.command(name='announce',
     description='Sends a message in a code block to the channel.')
+  @commands.has_role("Mods")
   async def announce(self, context, channel:discord.TextChannel, *, message:str = None):
     if message is None:
       await context.send('Please enter a message for me to send!')
@@ -25,6 +27,7 @@ class Mod:
 
   @commands.command(name='mute',
     description='Adds the \'Mute\' role to the member.')
+  @commands.has_role("Mods")
   async def mute(self, context, member:discord.Member):
     mute_role = discord.utils.find(lambda r: r.name == 'Mute', context.guild.roles)
     
@@ -36,6 +39,7 @@ class Mod:
 
   @commands.command(name='unmute',
     description='Removes the \'Mute\' role from the member.')
+  @commands.has_role("Mods")
   async def unmute(self, context, member:discord.Member):
     mute_role = discord.utils.find(lambda r: r.name == 'Mute', context.guild.roles)
     
@@ -47,6 +51,7 @@ class Mod:
 
   @commands.command(name='kick',
     description='Kicks a member from the server.')
+  @commands.has_role("Mods")
   async def kick(self, context, member:discord.Member, *, reason:str = None):
     try:
       await member.kick(reason=reason)
@@ -56,6 +61,7 @@ class Mod:
 
   @commands.command(name='ban',
     description='Bans a member from the server.')
+  @commands.has_role("Mods")
   async def ban(self, context, member:discord.Member, delete_message_days:int = 0, *, reason:str = None):
     try:
       await member.ban(reason=reason, delete_message_days=delete_message_days)
@@ -66,6 +72,7 @@ class Mod:
   @commands.command(name='pause',
     description='Disable sending messages to the channel.',
     aliases=['freeze'])
+  @commands.has_role("Mods")
   async def pause(self, context):
     everyone_role = context.guild.default_role
 
@@ -78,6 +85,7 @@ class Mod:
   @commands.command(name='resume',
     description='Enable sending messages to the channel (deletes overrides).',
     aliases=['unpause', 'unfreeze'])
+  @commands.has_role("Mods")
   async def resume(self, context):
     everyone_role = context.guild.default_role
 
