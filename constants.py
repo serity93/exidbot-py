b87fd99ce4d58064f1399f79f8de908a2dcd27b1
serity93 <serity93@gmail.com>
2020-11-09 21:18:40 -0800

    Beautify with auto formatter

--- constants.py
+++ constants.py
@@ -5,11 +5,11 @@ JSON_DATA_FILE = 'data.json'
 EXID_GUILD_ID = 170189590592880641
 
 # CHANNEL IDs
-BOT_TESTING_CHAN_ID     = 392513536594673674
-EXID_CHAN_ID            = 170189590592880641
-GENERAL_CHAN_ID         = 301888321683062784
-WELCOME_CHAN_ID         = 307367886303920128
-PICK_UR_BIAS_CHAN_ID    = 361985598304813066
+BOT_TESTING_CHAN_ID = 392513536594673674
+EXID_CHAN_ID = 170189590592880641
+GENERAL_CHAN_ID = 301888321683062784
+WELCOME_CHAN_ID = 307367886303920128
+PICK_UR_BIAS_CHAN_ID = 361985598304813066
 
 # DIRECTORIES
 GIF_DIR = 'gifs'
@@ -27,15 +27,26 @@ LE_FOLDER = '170obxkpILgejaLMDu5vyoTmzdBiz99xG'
 HANI_FOLDER = '1BkKKjOkybWr9MKOIUjjFE2mJ3GTBGEuh'
 HYELIN_FOLDER = '13zBgoWhjD3rDt7HQzdOrrXxK5eV56wEL'
 JEONGHWA_FOLDER = '1BTcAuyWs-PGUPhVR1-FABB1B1Tx1rZs7'
-GROUP_PIC_Q = "mimeType contains 'image/' and '{}' in parents".format(GROUP_FOLDER)
-SOLJI_PIC_Q = "mimeType contains 'image/' and '{}' in parents".format(SOLJI_FOLDER)
+GROUP_PIC_Q = "mimeType contains 'image/' and '{}' in parents".format(
+    GROUP_FOLDER)
+SOLJI_PIC_Q = "mimeType contains 'image/' and '{}' in parents".format(
+    SOLJI_FOLDER)
 LE_PIC_Q = "mimeType contains 'image/' and '{}' in parents".format(LE_FOLDER)
-HANI_PIC_Q = "mimeType contains 'image/' and '{}' in parents".format(HANI_FOLDER)
-HYELIN_PIC_Q = "mimeType contains 'image/' and '{}' in parents".format(HYELIN_FOLDER)
-JEONGHWA_PIC_Q = "mimeType contains 'image/' and '{}' in parents".format(JEONGHWA_FOLDER)
-GROUP_GIF_Q = "mimeType contains 'image/gif' and '{}' in parents".format(GROUP_FOLDER)
-SOLJI_GIF_Q = "mimeType contains 'image/gif' and '{}' in parents".format(SOLJI_FOLDER)
-LE_GIF_Q = "mimeType contains 'image/gif' and '{}' in parents".format(LE_FOLDER)
-HANI_GIF_Q = "mimeType contains 'image/gif' and '{}' in parents".format(HANI_FOLDER)
-HYELIN_GIF_Q = "mimeType contains 'image/gif' and '{}' in parents".format(HYELIN_FOLDER)
-JEONGHWA_GIF_Q = "mimeType contains 'image/gif' and '{}' in parents".format(JEONGHWA_FOLDER)
+HANI_PIC_Q = "mimeType contains 'image/' and '{}' in parents".format(
+    HANI_FOLDER)
+HYELIN_PIC_Q = "mimeType contains 'image/' and '{}' in parents".format(
+    HYELIN_FOLDER)
+JEONGHWA_PIC_Q = "mimeType contains 'image/' and '{}' in parents".format(
+    JEONGHWA_FOLDER)
+GROUP_GIF_Q = "mimeType contains 'image/gif' and '{}' in parents".format(
+    GROUP_FOLDER)
+SOLJI_GIF_Q = "mimeType contains 'image/gif' and '{}' in parents".format(
+    SOLJI_FOLDER)
+LE_GIF_Q = "mimeType contains 'image/gif' and '{}' in parents".format(
+    LE_FOLDER)
+HANI_GIF_Q = "mimeType contains 'image/gif' and '{}' in parents".format(
+    HANI_FOLDER)
+HYELIN_GIF_Q = "mimeType contains 'image/gif' and '{}' in parents".format(
+    HYELIN_FOLDER)
+JEONGHWA_GIF_Q = "mimeType contains 'image/gif' and '{}' in parents".format(
+    JEONGHWA_FOLDER)
--- main.py
+++ main.py
@@ -1,13 +1,13 @@
+from bottoken import *
+import traceback
+import sys
+import logging
 import discord
 from discord.ext import commands
 
-import logging
-import sys
-import traceback
 intents = discord.Intents.default()
 intents.members = True
 
-from bottoken import *
 
 """
 A Discord bot written specifically for the EXID Discord server.
@@ -22,23 +22,25 @@ Discord.py Rewrite Commands Documentation:
   http://discordpy.readthedocs.io/en/rewrite/ext/commands/api.html
 """
 
+
 def get_prefix(bot, message):
-  if not message.guild:
-    return '.'
+    if not message.guild:
+        return '.'
 
-  prefixes = ['.']
+    prefixes = ['.']
 
-  return commands.when_mentioned_or(*prefixes)(bot, message)
+    return commands.when_mentioned_or(*prefixes)(bot, message)
+
 
 initial_extensions = [
-  'cogs.owner',
-  'cogs.mod',
-  'cogs.events',
-  #'cogs.json_data',
-  'cogs.scheduler',
-  'cogs.member',
-  'cogs.random_pic',
-  ]
+    'cogs.owner',
+    'cogs.mod',
+    'cogs.events',
+    # 'cogs.json_data',
+    'cogs.scheduler',
+    'cogs.member',
+    'cogs.random_pic',
+]
 
 # CREATE BOT
 bot = commands.Bot(command_prefix=get_prefix,
@@ -49,16 +51,18 @@ bot = commands.Bot(command_prefix=get_prefix,
 # LOGGING
 logger = logging.getLogger('discord')
 logger.setLevel(logging.INFO)
-handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
-handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+handler = logging.FileHandler(
+    filename='discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter(
+    '%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
 
 if __name__ == '__main__':
-  for extension in initial_extensions:
-    try:
-      bot.load_extension(extension)
-    except Exception as e:
-      print(f'Failed to load extension {extension}.', file=sys.stderr)
-      traceback.print_exc()
+    for extension in initial_extensions:
+        try:
+            bot.load_extension(extension)
+        except Exception as e:
+            print(f'Failed to load extension {extension}.', file=sys.stderr)
+            traceback.print_exc()
 
 bot.run(TOKEN)
